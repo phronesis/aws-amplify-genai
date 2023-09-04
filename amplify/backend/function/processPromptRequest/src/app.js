00cf84d4ceed71a0d8fd3fb1d10c138e27c1de3f
David Umoh <daveumoh@amazon.com>
2023-09-04 16:09:11 +0100

    basic api functionality working as sagemaker is returning generated content

---
+++ amplify/backend/function/processPromptRequest/src/app.js
@@ -0,0 +1,122 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT *//*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const SAGEMAKER_ENDPOINT = ''; //TODO convert to ENV VAR
+// declare a new express app
+const app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+
+const { SageMakerRuntimeClient, InvokeEndpointCommand } = require("@aws-sdk/client-sagemaker-runtime")
+const sageMakerClient = new SageMakerRuntimeClient()
+
+
+/**********************
+ * Example get method *
+ **********************/
+
+app.get('/generator', function(req, res) {
+  // Add your code here
+  res.json({success: 'get call succeed!', url: req.url});
+});
+
+app.get('/generator/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'get call succeed!', url: req.url});
+});
+
+/****************************
+* Example post method *
+****************************/
+
+app.post('/generator', async function(req, res) {
+  
+  // const body = req.body
+  // const context = body.context
+  let postData = {
+    EndpointName: process.env.SAGEMAKER_ENDPOINT_NAME,
+    ContentType: "application/json",
+    Body: JSON.stringify({
+      "inputs": `Can you write an 80-120 words long children story about reading`,
+      "parameters": {
+        "max_new_tokens": 300,
+        "return_full_text": false,
+        "do_sample": true,
+        "top_k":10
+      }
+    })
+  }
+  console.log(process.env.SAGEMAKER_ENDPOINT_NAME)
+
+  //post data to sagemaker endpoint
+ const command = new InvokeEndpointCommand(postData)
+ const response =  await sageMakerClient.send(command)
+  //get response
+  const jsonString = Buffer.from(response.Body).toString('utf8')
+  const story = JSON.parse(jsonString)[0]['generated_text']
+  //process response and send back to client
+  return res.json({success: 'post call succeed!', url: req.url, body: story})
+});
+
+app.post('/generator/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+});
+
+/****************************
+* Example put method *
+****************************/
+
+app.put('/generator', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
+
+app.put('/generator/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
+
+/****************************
+* Example delete method *
+****************************/
+
+app.delete('/generator', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+app.delete('/generator/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,21 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apiCounderJHB": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "aicofounderbootstraplayer": {
+      "Arn": "string"
+    },
+    "processPromptRequest": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
